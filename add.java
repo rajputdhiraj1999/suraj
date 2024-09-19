2621e2bbfcabffa584ded0f8c68438eb7127c94b
Dhiraj Girase <rajputdhiraj1999>
2024-09-19 09:48:30 +0530

    this is my second commit

--- add.java
+++ add.java
@@ -3,5 +3,7 @@ class add
         public static void main(String[] args) 
         {
             System.out.println(10+20);
+            System.out.println(10+50);
+            System.out.println(10+40);
         }
     }
